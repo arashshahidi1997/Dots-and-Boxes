ef4bec5a8fdf46a2268a332e25ac9d8269464cd2
Arash <shahidi.arash@gmail.com>
2020-02-08 18:29:52 +0330

    enviornment and agent

---
+++ Dots and Boxes/agent.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class agent:
+
+    def __init__(self):
+        self.action = 0
+        self.policy = 0
+        self.value_function = 0
+
+    def update(self):
+        self.action = 0
+        self.policy = 0
+        self.value_function = 0
+
+class policy:
+
+    def __init__(self):
+
+
+
+class value_function:
+
+    def __init__(self):
